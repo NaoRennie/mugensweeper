e61193a354f64822a4690ea6242cb107d68d6edf
NaomiRennie <naomi.lily.405@gmail.com>
2018-12-21 09:18:33 +0900

    修正

--- api/tests/routes/dev/rennie/field.test.js
+++ api/tests/routes/dev/rennie/field.test.js
@@ -12,29 +12,29 @@ describe('field apiについてのテスト', () => {
     // Given ないこともあ
 
     // When
-    const { body } = await chai.request(app).get('/dev/rennie/field');
+    const { body } = await chai.request(app).get(`/dev/rennie/field`);
 
     // Then
     expect(body).toHaveLength(1);
     expect(body[0]).toMatchObject(initialBlock());
   });
 
-  // it('postした座標が返り値に追加される', async () => {
-  //   // Given
-  //   const position = {
-  //     x: 2,
-  //     y: 4,
-  //   };
+  it('postした座標が返り値に追加される', async () => {
+    // Given
+    const position = {
+      x: 2,
+      y: 4,
+    };
 
-  //   // When
-  //   const { body } = await chai
-  //     .request(app)
-  //     .post('/dev/rennie/block')
-  //     .set('content-type', 'application/x-www-form-urlencoded')
-  //     .send(position);
+    // When
+    const { body } = await chai
+      .request(app)
+      .post('/dev/rennie/block')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .send(position);
 
-  //   // Then
-  //   expect(body).toHaveLength(2);
-  //   expect(body).toEqual(expect.arrayContaining([initialBlock(), position]));
-  // });
+    // Then
+    expect(body).toHaveLength(2);
+    expect(body).toEqual(expect.arrayContaining([initialBlock(), position]));
+  });
 });
